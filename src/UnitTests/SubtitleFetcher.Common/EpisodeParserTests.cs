741d8d1379dfc9186cb154fe89bff479c05d31ca
pheiberg <peter@heiberg.se>
2015-11-03 20:14:20 +0100

    Added new EpisodeParser test case

--- src/UnitTests/SubtitleFetcher.Common/EpisodeParserTests.cs
+++ src/UnitTests/SubtitleFetcher.Common/EpisodeParserTests.cs
@@ -20,6 +20,7 @@ namespace UnitTests.SubtitleFetcher.Common
         [TestCase("Mythbusters S05E04 Speed Cameras WS DSR XviD-2SD", "Mythbusters", 5, 4, 4, "2SD")]
         [TestCase("be.cool.scooby-doo.s01e01.mystery.101.hdtv.x264-w4f", "be cool scooby-doo", 1, 1, 1, "w4f")]
         [TestCase("MultiEp.S01E01-E03.HDTV.x264-Group", "MultiEp", 1, 1, 3, "Group")]
+        [TestCase("Family.Guy.S14E02.Papa.Has.a.Rollin.Son.1080p.WEB-DL.DD5.1.H.264-CtrlHD", "Family Guy", 14, 2, 2, "CtrlHD")]
         public void Parse_ParsableName_PopulatedEpisodeInfo(string name, string seriesName, int season, int episode, int endEpisodeNumber, string releaseGroup)
         {
             var expected = new EpisodeIdentity {
