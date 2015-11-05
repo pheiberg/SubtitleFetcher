4ed2ba02b052716f96ccbc119fd6a33c9d6460cd
pheiberg <peter@heiberg.se>
2015-11-05 20:24:35 +0100

    Added test for ToHexString

---
+++ src/UnitTests/SubtitleFetcher.Common/ByteExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using SubtitleFetcher.Common;
+
+namespace UnitTests.SubtitleFetcher.Common
+{
+    [TestFixture]
+    public class ByteExtensionTests
+    {
+        [Test]
+        public void ToHexString_StaticData_CorrectHex()
+        {
+            var data = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+            var result = data.ToHexString();
+
+            Assert.That(result, Is.EqualTo("000102030405060708090A0B0C0D0E0F10"));
+        }
+    }
+}
