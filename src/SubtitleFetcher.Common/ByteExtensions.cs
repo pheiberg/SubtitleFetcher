44e0313da33a1d54cfb868c2dc4ae1159ecec22f
pheiberg <peter@heiberg.se>
2015-11-05 20:59:52 +0100

    Refactored ToUpperHexString

--- src/SubtitleFetcher.Common/ByteExtensions.cs
+++ src/SubtitleFetcher.Common/ByteExtensions.cs
@@ -1,17 +1,22 @@
-﻿using System.Text;
+﻿using System;
 
 namespace SubtitleFetcher.Common
 {
     public static class ByteExtensions
     {
-        public static string ToHexString(this byte[] bytes)
+        static readonly char[] HexDigits = "0123456789ABCDEF".ToCharArray();
+        
+        public static string ToUpperHexString(this byte[] bytes)
         {
-            var sb = new StringBuilder();
-            foreach (byte t in bytes)
+            var digits = new char[bytes.Length * 2];
+            for (var i = 0; i < bytes.Length; i++)
             {
-                sb.Append(t.ToString("X2"));
+                int digit2;
+                var digit1 = Math.DivRem(bytes[i], 16, out digit2);
+                digits[2 * i] = HexDigits[digit1];
+                digits[2 * i + 1] = HexDigits[digit2];
             }
-            return sb.ToString();
+            return new string(digits);
         }
     }
 }
--- src/UnitTests/SubtitleFetcher.Common/ByteExtensionTests.cs
+++ src/UnitTests/SubtitleFetcher.Common/ByteExtensionTests.cs
@@ -7,13 +7,23 @@ namespace UnitTests.SubtitleFetcher.Common
     public class ByteExtensionTests
     {
         [Test]
-        public void ToHexString_StaticData_CorrectHex()
+        public void ToHexString_StaticLowerData_CorrectHex()
         {
-            var data = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+            var data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 
-            var result = data.ToHexString();
+            var result = data.ToUpperHexString();
 
             Assert.That(result, Is.EqualTo("000102030405060708090A0B0C0D0E0F10"));
+        }
+
+        [Test]
+        public void ToHexString_StaticUpperData_CorrectHex()
+        {
+            var data = new byte[] { 255, 254, 253, 252, 251, 250, 249 };
+
+            var result = data.ToUpperHexString();
+
+            Assert.That(result, Is.EqualTo("FFFEFDFCFBFAF9"));
         }
     }
 }
