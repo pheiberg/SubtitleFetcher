4597e2591ff7b8148530ab27e27b6bc0d07f1c80
pheiberg <peter@heiberg.se>
2015-11-06 20:04:07 +0100

    Cleaned up FileProcessor

--- src/SubtitleFetcher/FileProcessor.cs
+++ src/SubtitleFetcher/FileProcessor.cs
@@ -11,20 +11,21 @@ namespace SubtitleFetcher
 {
     public class FileProcessor : IFileProcessor
     {
-        private readonly IEpisodeParser episodeParser;
-        private readonly ILogger logger;
-        private readonly ISubtitleDownloadService subtitleService;
-        private readonly IFileSystem fileSystem;
-        private readonly LanguageSettings languageSettings;
+        private readonly IEpisodeParser _episodeParser;
+        private readonly ILogger _logger;
+        private readonly ISubtitleDownloadService _subtitleService;
+        private readonly IFileSystem _fileSystem;
+        private readonly LanguageSettings _languageSettings;
         private readonly ISubDbHasher _hasher;
 
-        public FileProcessor(IEpisodeParser episodeParser, ILogger logger, ISubtitleDownloadService subtitleService, IFileSystem fileSystem, LanguageSettings languageSettings, ISubDbHasher hasher)
+        public FileProcessor(IEpisodeParser episodeParser, ILogger logger, ISubtitleDownloadService subtitleService, 
+                             IFileSystem fileSystem, LanguageSettings languageSettings, ISubDbHasher hasher)
         {
-            this.episodeParser = episodeParser;
-            this.logger = logger;
-            this.subtitleService = subtitleService;
-            this.fileSystem = fileSystem;
-            this.languageSettings = languageSettings;
+            _episodeParser = episodeParser;
+            _logger = logger;
+            _subtitleService = subtitleService;
+            _fileSystem = fileSystem;
+            _languageSettings = languageSettings;
             _hasher = hasher;
         }
 
@@ -33,26 +34,26 @@ namespace SubtitleFetcher
             var episodeIdentity = ParseReleaseIdentity(fileName);
             if (string.IsNullOrEmpty(episodeIdentity.SeriesName))
             {
-                logger.Error("File format", $"Can't parse episode info from {fileName}. Not on a known format.");
+                _logger.Error("File format", $"Can't parse episode info from {fileName}. Not on a known format.");
                 return true;
             }
 
             if (CheckIfShowIsIgnored(ignoredShows, episodeIdentity))
             {
-                logger.Verbose("FileProcessor", $"Ignoring {fileName}");
+                _logger.Verbose("FileProcessor", $"Ignoring {fileName}");
                 return true;
             }
 
             var languagesToDownload = DetermineLanguagesToDownload(fileName);
             if (!languagesToDownload.Any())
             {
-                logger.Verbose("FileProcessor", $"All languages already downloaded. Skipping {fileName}.");
+                _logger.Verbose("FileProcessor", $"All languages already downloaded. Skipping {fileName}.");
                 return true;
             }
             
             var languageList = string.Join(", ", languagesToDownload);
-            logger.Important("FileProcessor", $"Processing file {fileName}...");
-            logger.Debug("FileProcessor", $"Looking for subtitles in: {languageList}");
+            _logger.Important("FileProcessor", $"Processing file {fileName}...");
+            _logger.Debug("FileProcessor", $"Looking for subtitles in: {languageList}");
 
             return DownloadSubtitle(fileName, episodeIdentity, languagesToDownload);
         }
@@ -60,30 +61,34 @@ namespace SubtitleFetcher
         private string[] DetermineLanguagesToDownload(string fileName)
         {
             var dowloadedLanguages = GetAlreadyDownloadedLanguages(fileName);
-            return languageSettings.Languages.Except(dowloadedLanguages).ToArray();
+            return _languageSettings.Languages.Except(dowloadedLanguages).ToArray();
         }
 
         private TvReleaseIdentity ParseReleaseIdentity(string fileName)
         {
             var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-            var tvReleaseIdentity = episodeParser.ParseEpisodeInfo(fileNameWithoutExtension);
+            var tvReleaseIdentity = _episodeParser.ParseEpisodeInfo(fileNameWithoutExtension);
             tvReleaseIdentity.FileHash = _hasher.ComputeHash(fileName);
             return tvReleaseIdentity;
         }
 
-        private static bool CheckIfShowIsIgnored(IEnumerable<string> ignoredShows, TvReleaseIdentity tvReleaseIdentity)
+        private static bool CheckIfShowIsIgnored(IEnumerable<string> ignoredShows, 
+            TvReleaseIdentity tvReleaseIdentity)
         {
-            return ignoredShows.Any(s => string.Equals(s.RemoveNonAlphaNumericChars(), tvReleaseIdentity.SeriesName.RemoveNonAlphaNumericChars(), StringComparison.OrdinalIgnoreCase));
+            return ignoredShows.Any(s => string.Equals(s.RemoveNonAlphaNumericChars(), 
+                tvReleaseIdentity.SeriesName.RemoveNonAlphaNumericChars(), 
+                StringComparison.OrdinalIgnoreCase));
         }
 
         private IEnumerable<string> GetAlreadyDownloadedLanguages(string fileName)
         {
-            return fileSystem.GetDowloadedSubtitleLanguages(fileName, languageSettings.Languages);
+            return _fileSystem.GetDowloadedSubtitleLanguages(fileName, _languageSettings.Languages);
         }
 
-        private bool DownloadSubtitle(string fileName, TvReleaseIdentity tvReleaseIdentity, string[] languagesToDownload)
+        private bool DownloadSubtitle(string fileName, TvReleaseIdentity tvReleaseIdentity, 
+            IEnumerable<string> languagesToDownload)
         {
-            return subtitleService.DownloadSubtitle(fileName , tvReleaseIdentity, languagesToDownload);
+            return _subtitleService.DownloadSubtitle(fileName , tvReleaseIdentity, languagesToDownload);
         }
     }
 }
