ee3150d2600542780d0a03c241f64d706543f976
pheiberg <peter@heiberg.se>
2015-11-06 22:25:35 +0100

    Refactored DownloaderApplication

--- src/SubtitleFetcher/DownloaderApplication.cs
+++ src/SubtitleFetcher/DownloaderApplication.cs
@@ -1,31 +1,45 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SubtitleFetcher
 {
     public class DownloaderApplication
     {
-        private readonly IFileSystem fileSystem;
-        private readonly IStateSerializer serializer;
-        private readonly IFileProcessor fileProcessor;
+        private readonly IFileSystem _fileSystem;
+        private readonly IStateSerializer _serializer;
+        private readonly IFileProcessor _fileProcessor;
 
         public DownloaderApplication(IFileSystem fileSystem, IStateSerializer serializer, IFileProcessor fileProcessor)
         {
-            this.fileSystem = fileSystem;
-            this.serializer = serializer;
-            this.fileProcessor = fileProcessor;
+            _fileSystem = fileSystem;
+            _serializer = serializer;
+            _fileProcessor = fileProcessor;
         }
 
         public void Run(Options options)
         {
-            var ignoredShows = fileSystem.GetIgnoredShows(options.IgnoreFileName);
-            var state = serializer.LoadState();
-            state.Cleanup(options.GiveupDays, fileSystem.CreateNosrtFile);
+            var ignoredShows = _fileSystem.GetIgnoredShows(options.IgnoreFileName);
+            var state = _serializer.LoadState();
+            state.Cleanup(options.GiveupDays, _fileSystem.CreateNosrtFile);
 
-            var filesToProcess = fileSystem.GetFilesToProcess(options.Files, options.Languages);
-            var failedFiles = (filesToProcess.Where(file => !fileProcessor.ProcessFile(file, ignoredShows))).ToList();
+            var filesToProcess = _fileSystem.GetFilesToProcess(options.Files, options.Languages);
+            var failedFiles = ProcessFiles(filesToProcess, ignoredShows);
 
             state.Update(failedFiles);
-            serializer.SaveState(state);
+            _serializer.SaveState(state);
+        }
+
+        private IEnumerable<string> ProcessFiles(IEnumerable<string> filesToProcess, IEnumerable<string> ignoredShows)
+        {
+            var ignoredShowsArray = ignoredShows as string[] ?? ignoredShows.ToArray();
+            foreach (var file in filesToProcess)
+            {
+                bool success =_fileProcessor.ProcessFile(file, ignoredShowsArray);
+                if (!success)
+                {
+                    yield return file;
+                }
+            }
         }
     }
 }
