dd82e1e1ffe4e8895005e42a5fd666e86c54d4d0
pheiberg <peter@heiberg.se>
2015-11-13 19:09:35 +0100

    Simplified bootstrapping code

--- src/SubtitleFetcher/Bootstrapping/SubtitleDownloaderRegistry.cs
+++ src/SubtitleFetcher/Bootstrapping/SubtitleDownloaderRegistry.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using StructureMap;
 using StructureMap.Configuration.DSL;
 using StructureMap.Graph;
-using SubtitleFetcher.Common;
 using SubtitleFetcher.Common.Downloaders;
 using SubtitleFetcher.Common.Enhancement;
 using SubtitleFetcher.Common.Enhancement.Tvdb;
@@ -12,7 +10,6 @@ using SubtitleFetcher.Common.Infrastructure;
 using SubtitleFetcher.Common.Languages;
 using SubtitleFetcher.Common.Logging;
 using SubtitleFetcher.Common.Orchestration;
-using SubtitleFetcher.Common.Parsing;
 
 namespace SubtitleFetcher.Bootstrapping
 {
@@ -23,14 +20,7 @@ namespace SubtitleFetcher.Bootstrapping
             Scan(scan =>
             {
                 scan.AssembliesFromApplicationBaseDirectory();
-                if (options.DownloaderNames.Any())
-                {
-                    scan.With(new RegisterAllSubtitleDownloadersConvention(options.DownloaderNames));
-                }
-                else
-                {
-                    scan.AddAllTypesOf<ISubtitleDownloader>();
-                }
+                scan.With(new RegisterAllSubtitleDownloadersConvention(options.DownloaderNames));
                 scan.AddAllTypesOf<IEnhancer>();
             });
             Scan(scan =>
@@ -45,17 +35,9 @@ namespace SubtitleFetcher.Bootstrapping
             var languages = options.Languages.Select(KnownLanguages.GetLanguageByTwoLetterIso);
             For<LanguageSettings>().Use(new LanguageSettings(languages));
             For<FileTypeSettings>().Use(new FileTypeSettings(options.AcceptedExtensions));
-            For<ISubtitleDownloadService>().Use(CreateSubtitleDownloadService);
             For<EnhancerRegistry>().Singleton().Use<EnhancerRegistry>();
         }
-
-        private static SubtitleDownloadService CreateSubtitleDownloadService(IContext context)
-        {
-            Func<ISubtitleDownloader, SubtitleDownloaderWrapper> createSubtitleDownloader = sd => new SubtitleDownloaderWrapper(sd, context.GetInstance<IEpisodeParser>(), context.GetInstance<ILogger>(), context.GetInstance<IFileSystem>());
-            IEnumerable<SubtitleDownloaderWrapper> episodeSubtitleDownloaders = context.GetAllInstances<ISubtitleDownloader>().Select(createSubtitleDownloader);
-            return new SubtitleDownloadService(episodeSubtitleDownloaders, context.GetInstance<IEnhancementProvider>(), context.GetInstance<SubtitleRanker>());
-        }
-
+        
         private class RegisterAllSubtitleDownloadersConvention : IRegistrationConvention
         {
             private readonly IEnumerable<string> _downloaderNames;
@@ -67,11 +49,18 @@ namespace SubtitleFetcher.Bootstrapping
 
             public void Process(Type type, Registry registry)
             {
-                if (typeof(ISubtitleDownloader).IsAssignableFrom(type) && !type.IsAbstract && !type.IsInterface &&
-                    _downloaderNames.Any(d => type.Name.StartsWith(d, StringComparison.OrdinalIgnoreCase)))
-                {
-                    registry.For(typeof(ISubtitleDownloader)).Add(type);
-                }
+                bool isCompatibleType = typeof (ISubtitleDownloader).IsAssignableFrom(type)
+                                     && !type.IsAbstract
+                                     && !type.IsInterface;
+                bool isNameMatchingDownloaderOptions = !_downloaderNames.Any() 
+                                                    || !_downloaderNames.Any(d => type.Name.StartsWith(d, StringComparison.OrdinalIgnoreCase));
+                 
+                if (!isCompatibleType || !isNameMatchingDownloaderOptions)
+                    return;
+
+                registry.For(typeof(ISubtitleDownloader)).Add(type).Named(type.Name);
+                registry.For<IEpisodeSubtitleDownloader>().Add<SubtitleDownloaderWrapper>()
+                    .Ctor<ISubtitleDownloader>().IsNamedInstance(type.Name);
             }
         }
     }
