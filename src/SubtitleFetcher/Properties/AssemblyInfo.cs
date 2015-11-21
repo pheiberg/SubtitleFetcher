f026e7045f1ba0bc6be533f2022f3b86418a9ba5
pheiberg <peter@heiberg.se>
2015-11-21 16:58:31 +0100

    Removed copyright text

--- src/SubtitleFetcher/Properties/AssemblyInfo.cs
+++ src/SubtitleFetcher/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Subtitle Fetcher")]
-[assembly: AssemblyCopyright("Copyright ©2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
