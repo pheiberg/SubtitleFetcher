5172fc8c20c7ba60fb7047ce6077a3a3359e3c20
pheiberg <peter@heiberg.se>
2015-11-22 18:05:33 +0100

    OSDB hasher now handles empty files

--- src/SubtitleFetcher.Common/Downloaders/OpenSubtitles/Enhancement/OpenSubtitlesHashCalculator.cs
+++ src/SubtitleFetcher.Common/Downloaders/OpenSubtitles/Enhancement/OpenSubtitlesHashCalculator.cs
@@ -5,10 +5,15 @@ namespace SubtitleFetcher.Common.Downloaders.OpenSubtitles.Enhancement
 {
     public class OpenSubtitlesHashCalculator : IHashCalculator
     {
+        private const int BlockSize = 64*1024;
+
         public byte[] ComputeHash(byte[] buffer)
         {
+            if (buffer.Length < BlockSize)
+                return new byte[0];
+
             ulong lhash = 0;
-            for (int i = 0; i < 2 * 65536; i += 8)
+            for (int i = 0; i < 2 * BlockSize; i += 8)
             {
                 unchecked
                 {
---
+++ src/UnitTests/SubtitleFetcher.Common/Downloaders/OpenSubtitles/Enhancement/OpenSubtitlesHashCalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using SubtitleFetcher.Common.Downloaders.OpenSubtitles.Enhancement;
+
+namespace UnitTests.SubtitleFetcher.Common.Downloaders.OpenSubtitles.Enhancement
+{
+    [TestFixture]
+    public class OpenSubtitlesHashCalculatorTests
+    {
+        [Test, AutoFakeData]
+        public void ComputeHash_EmptyInput_ReturnsEmptyHash(
+            OpenSubtitlesHashCalculator sut)
+        {
+            var bytes = new byte[0];
+
+            var result = sut.ComputeHash(bytes);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test, AutoFakeData]
+        public void ComputeHash_InputSmallerThanBlockSize_ReturnsEmptyHash(
+            OpenSubtitlesHashCalculator sut)
+        {
+            var fixture = new Fixture { RepeatCount = new Random().Next(0, 63) };
+            var bytes = fixture.Create<byte[]>();
+
+            var result = sut.ComputeHash(bytes);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        public void ComputeHash_InputLargerThanBlockSize_ReturnsNonEmptyHash(
+            OpenSubtitlesHashCalculator sut)
+        {
+            var fixture = new Fixture { RepeatCount = new Random().Next(64, int.MaxValue) };
+            var bytes = fixture.Create<byte[]>();
+
+            var result = sut.ComputeHash(bytes);
+
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
+}
